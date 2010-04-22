627fa7592479528d009d6992b7b13cdb38e0d3a5
Ted Turocy <ted.turocy@gmail.com>
2010-04-22 16:27:30 -0400

    Stores probabilities of MLE estimate in returned mixed strategy profile.
    Augments returned STATA mixed strategy profile object by storing
    the probabilities associated with the MLE estimate.  The probabilities
    are stored in a nested array indexed by [player][strategy].
    
    In support of this:
    - Adds member program 'strategies' to game class.  Returns the number
      of strategies available to the player specified as the first argument.
    - Adds plugin function 'strategies' to support class member program.
    - Adds error checking on parameters to QRE call: verifies number of
      parameters is correct for the game, and that each parameter is a valid
      floating-point number.

--- src/stata/gambit.cc
+++ src/stata/gambit.cc
@@ -1,5 +1,6 @@
 #include <stdarg.h>
 #include <string.h>
+#include <math.h>
 #include <iostream>
 #include <fstream>
 #include <exception>
@@ -34,6 +35,27 @@ int gambit_atoi(const char *s)
   }
   if (!is.eof()) {
     throw ValueError(std::string(s) + " not a valid integer value");
+  }
+
+  return x;
+}
+
+// Convert a string to double, raising a ValueError exception if the
+// string does not contain a valid integer representation
+double gambit_atof(const char *s)
+{
+  std::string ss(s);
+  std::istringstream is(ss);
+  is.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+  double x;
+  try {
+    is >> x;
+  }
+  catch (std::ifstream::failure e) {
+    throw ValueError(std::string(s) + " not a valid floating-point value");
+  }
+  if (!is.eof()) {
+    throw ValueError(std::string(s) + " not a valid floating-point value");
   }
 
   return x;
@@ -75,17 +97,21 @@ inline ST_retcode stata_scal_save(const char *scal, double v)
 class STATATracer : public StrategicQREPathTracer {
 public:
   STATATracer(const MixedStrategyProfile<double> &p_start)
-    : StrategicQREPathTracer(p_start), m_mleLambda(-1.0), m_mleLogLike(0.0) { }
+    : StrategicQREPathTracer(p_start), 
+      m_mleLambda(-1.0), m_mleLogLike(0.0), m_mleProfile(p_start) { }
   virtual ~STATATracer() { }
 
   double GetMLELambda(void) const { return m_mleLambda; }
   double GetMLELogLike(void) const { return m_mleLogLike; }
+  const MixedStrategyProfile<double> &GetMLEProfile(void) const
+  { return m_mleProfile; }
 
 protected:
   virtual void OnStep(const Vector<double> &, bool);
 
 private:
   double m_mleLambda, m_mleLogLike;
+  MixedStrategyProfile<double> m_mleProfile;
 };
 
 void STATATracer::OnStep(const Vector<double> &p_point, bool p_isTerminal)
@@ -100,6 +126,9 @@ void STATATracer::OnStep(const Vector<double> &p_point, bool p_isTerminal)
   if (m_mleLambda < 0.0 || logL > m_mleLogLike) {
     m_mleLambda = p_point[p_point.Length()];
     m_mleLogLike = logL;
+    for (int i = 1; i <= m_mleProfile.Length(); i++) {
+      m_mleProfile[i] = exp(p_point[i]);
+    }
   }
 }
 
@@ -108,19 +137,38 @@ ST_retcode compute_qre(Gambit::List<Gambit::Game> &handles, int argc, char *argv
   double maxLambda = 1000000.0;
 
   Gambit::Game game = handles[atoi(argv[1])];
+
+  if (argc != game->MixedProfileLength() + 2) {
+    stata_error("qre requires one probability or frequency for each strategy.\n");
+    return (ST_retcode) 198;
+  }
+
   Gambit::MixedStrategyProfile<double> start(game);
   Gambit::Array<double> frequencies(game->MixedProfileLength());
   for (int i = 1; i <= frequencies.Length(); i++) {
-    frequencies[i] = atof(argv[i+1]);
+    try {
+      frequencies[i] = gambit_atof(argv[i+1]);
+    }
+    catch (ValueError e) {
+      stata_error("invalid floating-point number '%s'.\n", argv[i+1]);
+      return (ST_retcode) 198;
+    }
   }
 
   STATATracer tracer(start);
   tracer.SetMLEFrequencies(frequencies);
   tracer.TraceStrategicPath(start, 0.0, maxLambda, 1.0);
-  //stata_display("MLE lambda: %f\n", tracer.GetMLELambda());
 
   stata_scal_save("_lambda", tracer.GetMLELambda());
   stata_scal_save("_logL", tracer.GetMLELogLike());
+  for (int pl = 1; pl <= game->NumPlayers(); pl++) {
+    GamePlayer player = game->GetPlayer(pl);
+    for (int st = 1; st <= player->NumStrategies(); st++) {
+      std::ostringstream os;
+      os << "_prob_" << pl << "_" << st;
+      stata_scal_save(os.str().c_str(), tracer.GetMLEProfile()[player->GetStrategy(st)]);
+    }
+  }
 
   return (ST_retcode) 0;
 }
@@ -188,6 +236,36 @@ ST_retcode load_game(Gambit::List<Gambit::Game> &handles, char *path)
 ST_retcode count_players(const Gambit::Game &p_game)
 {
   stata_scal_save("_countplayers", p_game->NumPlayers());
+  return (ST_retcode) 0;
+}
+
+ST_retcode count_strategies(Gambit::List<Gambit::Game> &handles,
+			    int argc, char *argv[])
+{
+  if (argc != 3) {
+    stata_error("Exactly 2 arguments required for strategies.\n");
+    return (ST_retcode) 198;
+  }
+
+  Gambit::Game game = handles[atoi(argv[1])];
+
+  int pl;
+
+  try {
+    pl = gambit_atoi(argv[2]);
+  }
+  catch (ValueError e) {
+    stata_error("invalid player index '%s'.\n", argv[2]);
+    return (ST_retcode) 198;
+  }
+
+  if (pl < 1 || pl > game->NumPlayers()) {
+    stata_error("game has '%d' players; player '%d' specified.\n",
+		game->NumPlayers(), pl);
+    return (ST_retcode) 198;
+  }
+
+  stata_scal_save("_countstrategies", game->GetPlayer(pl)->NumStrategies());
   return (ST_retcode) 0;
 }
 
@@ -358,6 +436,13 @@ STDLL stata_call(int argc, char *argv[])
       return ((ST_retcode) 198);
     }
     return count_players(handles[atoi(argv[1])]);
+  }
+  else if (!strcmp(argv[0], "strategies")) {
+    if (argc == 1) {
+      stata_error("strategies command requires handle argument\n");
+      return ((ST_retcode) 198);
+    }
+    return count_strategies(handles, argc, argv);
   }
   else if (!strcmp(argv[0], "qre")) {
     if (argc == 1) {
