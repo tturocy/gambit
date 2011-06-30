3089f44f00aaf77aba952b9286d9c3a742bf2782
Ted Turocy <ted.turocy@gmail.com>
2011-06-30 17:47:10 -0400

    Remove debugging output.

--- src/stata/gambit.cc
+++ src/stata/gambit.cc
@@ -241,7 +241,6 @@ ST_retcode compute_qre_eval(const Gambit::Game &game, int argc, char *argv[])
     stata_error("qre_eval requires target lambda parameter.\n");
     return (ST_retcode) 198;
   }
-  stata_display("number of arguments: %d\n", argc);
   try {
     targetLambda = gambit_atof(argv[2]);
   }
